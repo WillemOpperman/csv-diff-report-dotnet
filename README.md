ca9c5d65f39753eaeb200689e5b7a9de282e299d
willemopperman <willem.n.opperman@gmail.com>
2023-10-07 14:41:34 +0200

    feat: add initial CSVDiffReport

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# csv-diff-report-dotnet
+# CSVDiffReport
+
+[![Sponsors on Open Collective](https://opencollective.com/csvdiff/sponsors/badge.svg)](#sponsors)
+<a href="https://www.nuget.org/packages/CSVDiffReport"><img src="https://img.shields.io/nuget/v/CSVDiffReport.svg" alt="NuGet Version" /></a>
+<a href="https://www.nuget.org/packages/CSVDiffReport"><img src="https://img.shields.io/nuget/dt/CSVDiffReport.svg" alt="NuGet Download Count" /></a>
+
+A library for producing HTML|CSV|EXCEL reports using <a href="https://github.com/WillemOpperman/csv-diff-dotnet">CSVDiff</a>.
+
+## Install
+
+### Package Manager Console
+
+```
+PM> Install-Package CSVDiffReport
+```
+
+### .NET CLI Console
+
+```
+> dotnet add package CSVDiffReport
+```
+
+
+## Credits
+
+### Inspiration
+
+This library would not be possible without the work of Andrew Gardiner and his project:
+https://github.com/agardiner/csv-diff-report
+
+### Contributors
+
+This project exists thanks to all the people who contribute.
+
+<a href="https://github.com/WillemOpperman/csv-diff-report-dotnet/graphs/contributors"><img src="https://opencollective.com/csvdiff/contributors.svg?width=890&button=false" /></a>
+
+### Backers
+
+Thank you to all our backers! 🙏 [[Become a backer](https://opencollective.com/csvdiff#backer)]
+
+<a href="https://opencollective.com/csvdiff#backers" target="_blank"><img src="https://opencollective.com/csvdiff/backers.svg?width=890"></a>
+
+### Sponsors
+
+Support this project by becoming a sponsor. Your logo will show up here with a link to your website. [[Become a sponsor](https://opencollective.com/csvdiff#sponsor)]
+
+<a href="https://opencollective.com/csvdiff/sponsor/0/website" target="_blank"><img src="https://opencollective.com/csvdiff/sponsor/0/avatar.svg"></a>
---
+++ tests/TestReport.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using csv_diff;
+using csv_diff_report;
+using CsvHelper;
+using Xunit;
+
+namespace tests;
+
+public class TestReport
+{
+    private static readonly List<string[]> Data1 = new()
+    {
+        new[] { "Parent", "Child", "Description" },
+        new[] { "A", "A1", "Account1" },
+        new[] { "A", "A2", "Account 2" },
+        new[] { "A", "A3", "Account 3" },
+        new[] { "A", "A4", "Account 4" },
+        new[] { "A", "A6", "Account 6" }
+    };
+
+    private static readonly List<string[]> Data2 = new()
+    {
+        new[] { "Parent", "Child", "Description" },
+        new[] { "A", "A1", "Account1" },
+        new[] { "A", "A2", "Account2" },
+        new[] { "A", "a3", "ACCOUNT 3" },
+        new[] { "A", "A5", "Account 5" },
+        new[] { "B", "A6", "Account 6" },
+        new[] { "C", "A6", "Account 6c" }
+    };
+    
+    [Fact]
+    public void TestExcludeFilter()
+    {
+        CSVSource source1 = new CSVSource(Data1, new Dictionary<string, object>
+        {
+            { "key_fields", new List<int> { 0, 1 } },
+            { "exclude", new Dictionary<string, Regex> { { "Description", new Regex("Account\\d") } } }
+        });
+
+        CSVSource source2 = new CSVSource(Data2, new Dictionary<string, object>
+        {
+            { "key_fields", new List<int> { 0, 1 } },
+            { "exclude", new Dictionary<string, Regex> { { "2", new Regex("^ACC") } } }
+        });
+
+        var diff = new CSVDiff(source1, source2, new Dictionary<string, object>
+        {
+            { "parent_field", 0 },
+            { "child_field", 1 }
+        });
+
+        var textOutputFilePath = Path.GetTempFileName();
+        var textReport = new Text();
+        textReport.Add(diff);
+        textReport.TextOutput(textOutputFilePath);
+        
+        var htmlOutputFilePath = Path.GetTempFileName();
+        var htmlReport = new Html();
+        htmlReport.Add(diff);
+        htmlReport.HtmlOutput(htmlOutputFilePath);
+
+        
+        
+
+        Assert.Equal(1, source1.SkipCount);
+        Assert.Equal(1, source2.SkipCount);
+    }
+}
